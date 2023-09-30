676ee5317ca37926f893b21f102d1cec94d7f5de
hsperez6 <hsperez@gmail.com>
2023-09-29 21:29:36 -0700

    second update

--- app.js
+++ app.js
@@ -1,42 +1,80 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 app.set('views', './views');
-app.use(bodyParser.urlencoded({ extended: false }));
 
 
-const colors = [
-  'red',
-  'orange',
-  'yellow',
-  'green',
-  'blue',
-  'purple'
-];
+// Routes *****************************************
 
-
-// Routes
-app.get('/', (req, res) => {
-  res.render('index')
+app.use((req, res, next) => {
+  console.log('Hello');
+  const err = new Error('Oh no!');
+  next();
 });
 
-app.get('/cards', (req, res) => {
-  res.render('card', { prompt: "Who is buried in Grant's tomb?", colors })
+app.use((req, res, next) => {
+  console.log('World');
+  next();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {  
+  const name = req.cookies.username;
+  if ( name ) {
+    res.render('index', { name });
+  } else {
+    res.redirect('/hello');
+  }
+});
+
+app.post('/goodbye', (req, res) => {  
+  res.clearCookie('username');
+  res.redirect('/hello');
 });
 
 app.get('/hello', (req, res) => {
-  res.render('hello')
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/')
+  } else {
+    res.render('hello')
+  }
 });
 
-app.post('/hello', (req, res) => {
-  console.dir(req.body)
-  res.render('hello')
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/');
 });
 
+app.get('/cards', (req, res) => {
+  res.render('card', { prompt: "Who is buried in Grant's tomb?" })
+});
+
+
+// Server **************************************************
 app.listen(3000, () => {
   console.log('App is running on localhost:3000')
 });
